b62908335ed84b51e6c99aa441519958eaad49b1
Michal Wilkosz <mw7883475@gmail.com>
2023-12-26 03:41:45 +0100

    taskmanager singleton

--- ProjectsManagement/TaskManager.cpp
+++ ProjectsManagement/TaskManager.cpp
@@ -1,1 +1,11 @@
 #include "TaskManager.h"
+
+TaskManager* TaskManager::m_taskManager = nullptr;
+
+TaskManager* TaskManager::getInstance()
+{
+	if (m_taskManager == nullptr)
+		m_taskManager = new TaskManager();
+
+	return m_taskManager;
+}
--- ProjectsManagement/TaskManager.h
+++ ProjectsManagement/TaskManager.h
@@ -1,5 +1,20 @@
 #pragma once
 class TaskManager
 {
+protected:
+	static TaskManager* m_taskManager;
+protected:
+	TaskManager() {
+
+	}
+public:
+	TaskManager(TaskManager &other) = delete;
+	TaskManager(TaskManager &&other) = delete;
+	void operator=(TaskManager& other) = delete;
+	void operator=(TaskManager&& other) = delete;
+
+	static TaskManager* getInstance();
+
+	//void addUserToTask();
 };
 
