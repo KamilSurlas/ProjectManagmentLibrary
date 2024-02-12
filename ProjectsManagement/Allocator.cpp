8b39c146953628ac4bd390fc50a6486cb62bca63
Michal <mw7883475@gmail.com>
2024-02-13 00:10:24 +0100

    allocator

---
+++ ProjectsManagement/Allocator.cpp
@@ -0,0 +1,53 @@
+#include "Allocator.h"
+
+template<typename T, int limit>
+Allocator<T, limit>::Allocator() {
+    this->data = new T[limit];
+    counter = 0;
+}
+
+template<typename T, int limit>
+Allocator<T, limit>::~Allocator() {
+    delete[] this->data;
+}
+
+template<typename T, int limit>
+void Allocator<T, limit>::realloc(int newLimit) {
+    if (newLimit <= 0) {
+        throw std::invalid_argument("Wrong argument")
+    }
+    
+    T* temp = new T[newLimit];
+
+    if (temp == nullptr) {
+        throw std::invalid_argument("Failed allocation");
+    }
+
+    for (int i = 0; i < limit; i++) {
+        temp[i] = data[i];
+    }
+
+    delete[] data;
+    data = temp;
+    temp = nullptr;
+    limit = newLimit;
+}
+
+template<typename T, int limit>
+void Allocator<T, limit>::addElement(T& element) {
+    this->data[counter] = element;
+    counter++;
+    
+    if (counter >= limit)
+        realloc(limit + 100);
+}
+
+template<typename T, int limit>
+T& Allocator<T, limit>::operator[](int idx) {
+    if (0 <= idx && idx < limit) {
+        return this->data[idx];
+    }
+    else {
+        throw std::out_of_range("Provided index is out of range");
+    }
+}
